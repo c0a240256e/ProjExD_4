13370ad728c8e1e5a4acd5ae116969ab679da544
Yae <c0a2426126@edu.teu.ac.jp>
2025-12-09 17:43:48 +0900

    追加機能６実装完了

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -141,14 +141,14 @@ class Beam(pg.sprite.Sprite):
     """
     ビームに関するクラス
     """
-    def __init__(self, bird: Bird):
+    def __init__(self, bird: Bird, angle0 = 0):
         """
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん
         """
         super().__init__()
         self.vx, self.vy = bird.dire
-        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        angle = math.degrees(math.atan2(-self.vy, self.vx)) + angle0
         self.image = pg.transform.rotozoom(pg.image.load(f"fig/beam.png"), angle, 1.0)
         self.vx = math.cos(math.radians(angle))
         self.vy = -math.sin(math.radians(angle))
@@ -165,6 +165,31 @@ class Beam(pg.sprite.Sprite):
         self.rect.move_ip(self.speed*self.vx, self.speed*self.vy)
         if check_bound(self.rect) != (True, True):
             self.kill()
+            
+class NeoBeam():
+    """
+    左シフトを押したままスペースキーを押すと複数射出する。
+    """
+    def __init__(self, bird: Bird, num: int):
+        self.bird = bird
+        self.num = num
+        
+    def gen_beams(self):
+        beams = []
+        
+        if self.num == 1:
+            angles = [0]
+        else:
+            step = 100 / (self.num - 1)
+            angles = range(-50, +51, int(step))
+            
+        for angle in angles:
+            beam = Beam(self.bird, angle)
+            beams.append(beam)
+            
+        return beams
+            
+        
 
 
 class Explosion(pg.sprite.Sprite):
@@ -242,35 +267,6 @@ class Score:
         screen.blit(self.image, self.rect)
 
 
-class Shield(pg.sprite.Sprite):
-    """
-    こうかとんの前に防御壁を出現させ、着弾を防ぐ
-    発動時間:400フレーム
-    発動条件:「s」キー押下、スコアが50以上、防御壁が他に存在しない
-    消費スコア:50
-    """
-    def __init__(self, bird: Bird, life: int):
-        super().__init__()
-        self.life = life
-        width = 20
-        height = bird.rect.height * 2
-        self.image = pg.Surface((width,height))  # 空のSurfaceを生成
-        pg.draw.rect(self.image, (0,0,255),(0,0,width,height))  # 防御壁を生成
-        vx, vy = bird.dire  # こうかとんの向きを取得する
-        angle = math.degrees(math.atan2(-vy,vx))  # 角度を求める
-        self.image = pg.transform.rotozoom(self.image, angle, 1.0)  # 向きに合わせて防御壁を回転させる
-        self.rect = self.image.get_rect()
-        self.rect.centerx = bird.rect.centerx + bird.rect.width * vx  # こうかとんの中心からこうかとん1体分ずらした位置に配置
-        self.rect.centery = bird.rect.centery + bird.rect.height * vy
-        self.image.set_colorkey((0,0,0))
-
-    def update(self):
-        self.life -= 1
-        if self.life < 0:  # ライフが0未満になったら消滅
-            self.kill()
-
-
-
 def main():
     pg.display.set_caption("真！こうかとん無双")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -282,21 +278,19 @@ def main():
     beams = pg.sprite.Group()
     exps = pg.sprite.Group()
     emys = pg.sprite.Group()
-    shields = pg.sprite.Group()  # 防御壁
 
     tmr = 0
     clock = pg.time.Clock()
     while True:
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                beams.add(Beam(bird))
-            if event.type == pg.KEYDOWN and event.key == pg.K_s:
-                if score.value >= 50 and len(shields) == 0:
-                    shields.add(Shield(bird, 400))
-                    score.value -= 50
+                if key_lst[pg.K_LSHIFT]:
+                    nb = NeoBeam(bird, 5)
+                    for beam in nb.gen_beams():  
+                        beams.add(beam)
+                else:
+                    beams.add(Beam(bird))
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
@@ -322,9 +316,6 @@ def main():
             pg.display.update()
             time.sleep(2)
             return
-        
-        for bomb in pg.sprite.groupcollide(bombs,shields,True,False).keys():  # 防御壁と衝突した爆弾リスト
-            exps.add(Explosion(bomb, 50))  # 爆発エフェクト
 
         bird.update(key_lst, screen)
         beams.update()
@@ -336,8 +327,6 @@ def main():
         exps.update()
         exps.draw(screen)
         score.update(screen)
-        shields.update()
-        shields.draw(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
